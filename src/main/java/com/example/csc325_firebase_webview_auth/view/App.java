c29a135f4da979630806f16f0f71f6f79d794238
aohal <aohalloran716@gmail.com>
2025-06-27 16:00:54 -0400

    GUI upda

--- src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
@@ -1,6 +1,5 @@
 package com.example.csc325_firebase_webview_auth.view;
 
-
 import com.example.csc325_firebase_webview_auth.model.FirestoreContext;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,12 +9,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
-
     public static Firestore fstore;
     public static FirebaseAuth fauth;
     public static Scene scene;
@@ -23,12 +20,33 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        fstore = contxtFirebase.firebase();
-        fauth = FirebaseAuth.getInstance();
-        scene = new Scene(loadFXML("/files/AccessFBView.fxml"));
-        primaryStage.setScene(scene);
+        // Show splash screen first
+        Parent splashRoot = loadFXML("/files/Splash.fxml");
+        Scene splashScene = new Scene(splashRoot);
+        primaryStage.setScene(splashScene);
         primaryStage.show();
+
+        // After delay, load main app
+        new Thread(() -> {
+            try {
+                Thread.sleep(2500); // 2.5s splash duration
+
+                // Init Firebase
+                fstore = contxtFirebase.firebase();
+                fauth = FirebaseAuth.getInstance();
+
+                Parent mainRoot = loadFXML("/files/AccessFBView.fxml");
+                javafx.application.Platform.runLater(() -> {
+                    scene = new Scene(mainRoot);
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
+
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
@@ -42,5 +60,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
 }
+
